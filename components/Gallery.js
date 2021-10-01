6987c5f01f04b51a5b41c73a496c8fd559d652c7
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-10-01 14:02:13 -0700

    added 3 main components

---
+++ components/Gallery.js
@@ -0,0 +1,9 @@
+function Gallery(props) {
+  return (
+    <div>
+      <h2>Gallery</h2>
+    </div>
+  );
+}
+
+export default Gallery;
---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+function Nav(props) {
+  return (
+    <nav>
+      <h2>Nav</h2>
+    </nav>
+  );
+}
+
+export default Nav;
